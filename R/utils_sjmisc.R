29d4f621034f27559f225a8acb0e38bec17a1177
Indrajeet Patil <inderonline1988@gmail.com>
2021-06-08 08:25:04 +0200

    port over sjmisc and sjlabelled helpers; #14

---
+++ R/utils_sjmisc.R
@@ -0,0 +1,36 @@
+string_starts_with <- function(pattern, x) {
+  pattern <- paste0("^\\Q", pattern, "\\E")
+  grep(pattern, x, perl = TRUE)
+}
+
+string_contains <- function(pattern, x) {
+  pattern <- paste0("\\Q", pattern, "\\E")
+  grep(pattern, x, perl = TRUE)
+}
+
+string_ends_with <- function(pattern, x) {
+  pattern <- paste0("\\Q", pattern, "\\E$")
+  grep(pattern, x, perl = TRUE)
+}
+
+string_one_of <- function(pattern, x) {
+  insight::check_if_installed("purrr")
+  m <- unlist(purrr::map(pattern, ~ grep(., x, fixed = TRUE, useBytes = TRUE)))
+  x[m]
+}
+
+rownames_as_column <- function(x, var = "rowname") {
+  rn <- data.frame(rn = rownames(x), stringsAsFactors = FALSE)
+  x <- cbind(rn, x)
+  colnames(x)[1] <- var
+  rownames(x) <- NULL
+  x
+}
+
+obj_has_name <- function(x, name) {
+  name %in% names(x)
+}
+
+obj_has_rownames <- function(x) {
+  !identical(as.character(1:nrow(x)), rownames(x))
+}
