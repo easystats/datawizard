389e477edf611e7d96c01f7384011dbd15f49bef
Indrajeet Patil <inderonline1988@gmail.com>
2021-06-23 13:52:27 +0200

    add tests for winsorization

---
+++ tests/testthat/test-winsorization.R
@@ -0,0 +1,9 @@
+
+test_that("testing Winsorization of factors", {
+  expect_equal(winsorize(as.factor(mtcars$am)), as.factor(mtcars$am))
+})
+
+test_that("with missing values", {
+  expect_snapshot(winsorize(na.omit(ggplot2::msleep$brainwt)))
+  expect_equal(length(winsorize(as.factor(ggplot2::msleep$vore))), 83)
+})
