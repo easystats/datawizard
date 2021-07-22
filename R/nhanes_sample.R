b215e9f4fc175267944194238455b5faa44e47fe
Daniel <mail@danielluedecke.de>
2021-07-22 12:29:28 +0200

    docs for data

---
+++ R/nhanes_sample.R
@@ -0,0 +1,10 @@
+#' @docType data
+#' @title Sample dataset from the National Health and Nutrition Examination Survey
+#' @name nhanes_sample
+#' @keywords data
+#'
+#' @description Selected variables from the National Health and Nutrition Examination
+#'              Survey that are used in the example from Lumley (2010), Appendix E.
+#'
+#' @references Lumley T (2010). Complex Surveys: a guide to analysis using R. Wiley
+NULL
