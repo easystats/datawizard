0739171d9f268bab9e793c8a0c2f2b93b01d668d
Daniel <mail@danielluedecke.de>
2021-11-04 12:46:53 +0100

    Update test-std_center.R

--- tests/testthat/test-std_center.R
+++ tests/testthat/test-std_center.R
@@ -6,19 +6,23 @@ if (require("testthat")) {
   test_that("standardize.data.frame", {
     x <- standardise(d, select = c("Sepal.Length", "Sepal.Width"))
     expect_equal(as.vector(x$Sepal.Length), as.vector(scale(d$Sepal.Length)), tolerance = 0.001)
+    expect_equal(as.vector(x$Petal.Length), as.vector(d$Petal.Length), tolerance = 0.001)
 
     x <- standardise(d, select = c("Sepal.Length", "Sepal.Width"), append = TRUE)
     expect_equal(as.vector(x$Sepal.Length_z), as.vector(scale(d$Sepal.Length)), tolerance = 0.001)
     expect_equal(as.vector(x$Sepal.Length), as.vector(d$Sepal.Length), tolerance = 0.001)
+    expect_equal(as.vector(x$Petal.Length), as.vector(d$Petal.Length), tolerance = 0.001)
   })
 
   # standardize -----------------------------------------------------
   test_that("center.data.frame", {
     x <- center(d, select = c("Sepal.Length", "Sepal.Width"))
     expect_equal(as.vector(x$Sepal.Length), as.vector(d$Sepal.Length - mean(d$Sepal.Length)), tolerance = 0.001)
+    expect_equal(as.vector(x$Petal.Length), as.vector(d$Petal.Length), tolerance = 0.001)
 
     x <- center(d, select = c("Sepal.Length", "Sepal.Width"), append = TRUE)
     expect_equal(as.vector(x$Sepal.Length_c), as.vector(d$Sepal.Length - mean(d$Sepal.Length)), tolerance = 0.001)
     expect_equal(as.vector(x$Sepal.Length), as.vector(d$Sepal.Length), tolerance = 0.001)
+    expect_equal(as.vector(x$Petal.Length), as.vector(d$Petal.Length), tolerance = 0.001)
   })
 }
