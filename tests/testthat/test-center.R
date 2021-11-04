a167cb9c7149f5302397b6c4cb18452558d6359a
Daniel <mail@danielluedecke.de>
2021-11-04 21:21:30 +0100

    Update test-center.R

--- tests/testthat/test-center.R
+++ tests/testthat/test-center.R
@@ -3,16 +3,16 @@ if (require("testthat")) {
   data(mtcars)
   z <- center(iris$Sepal.Width)
   test_that("center", {
-    expect_equal(z, iris$Sepal.Width - mean(iris$Sepal.Width))
+    expect_equal(as.vector(z), iris$Sepal.Width - mean(iris$Sepal.Width), tolerance = 1e-4, ignore_attr = TRUE)
   })
   z <- center(mtcars$hp, robust = TRUE)
   test_that("center, robust", {
-    expect_equal(z, mtcars$hp - median(mtcars$hp))
+    expect_equal(as.vector(z), mtcars$hp - median(mtcars$hp), tolerance = 1e-4, ignore_attr = TRUE)
   })
 
   z <- center(iris, select = "Sepal.Width")
   test_that("center, select", {
-    expect_equal(z$Sepal.Width, iris$Sepal.Width - mean(iris$Sepal.Width))
+    expect_equal(as.vector(z$Sepal.Width), iris$Sepal.Width - mean(iris$Sepal.Width), tolerance = 1e-4, ignore_attr = TRUE)
   })
 
   z <- center(iris, select = "Species")
@@ -23,7 +23,7 @@ if (require("testthat")) {
   z <- center(iris, select = "Species", force = TRUE)
   v <- as.numeric(iris$Species)
   test_that("center, force factors", {
-    expect_equal(z$Species, v - median(v))
+    expect_equal(as.vector(z$Species), v - median(v), tolerance = 1e-4, ignore_attr = TRUE)
   })
 
   test_that("center, all na", {
