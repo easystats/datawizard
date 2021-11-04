d5caec3372c1eb302812c41dd0b4db74b691ae68
Daniel <mail@danielluedecke.de>
2021-11-04 12:48:29 +0100

    Update test-std_center.R

--- tests/testthat/test-std_center.R
+++ tests/testthat/test-std_center.R
@@ -7,11 +7,13 @@ if (require("testthat")) {
     x <- standardise(d, select = c("Sepal.Length", "Sepal.Width"))
     expect_equal(as.vector(x$Sepal.Length), as.vector(scale(d$Sepal.Length)), tolerance = 0.001)
     expect_equal(as.vector(x$Petal.Length), as.vector(d$Petal.Length), tolerance = 0.001)
+    expect_equal(colnames(x), c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species"))
 
     x <- standardise(d, select = c("Sepal.Length", "Sepal.Width"), append = TRUE)
     expect_equal(as.vector(x$Sepal.Length_z), as.vector(scale(d$Sepal.Length)), tolerance = 0.001)
     expect_equal(as.vector(x$Sepal.Length), as.vector(d$Sepal.Length), tolerance = 0.001)
     expect_equal(as.vector(x$Petal.Length), as.vector(d$Petal.Length), tolerance = 0.001)
+    expect_equal(colnames(x), c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species", "Sepal.Length_z", "Sepal.Width_z"))
   })
 
   # standardize -----------------------------------------------------
@@ -19,10 +21,12 @@ if (require("testthat")) {
     x <- center(d, select = c("Sepal.Length", "Sepal.Width"))
     expect_equal(as.vector(x$Sepal.Length), as.vector(d$Sepal.Length - mean(d$Sepal.Length)), tolerance = 0.001)
     expect_equal(as.vector(x$Petal.Length), as.vector(d$Petal.Length), tolerance = 0.001)
+    expect_equal(colnames(x), c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species"))
 
     x <- center(d, select = c("Sepal.Length", "Sepal.Width"), append = TRUE)
     expect_equal(as.vector(x$Sepal.Length_c), as.vector(d$Sepal.Length - mean(d$Sepal.Length)), tolerance = 0.001)
     expect_equal(as.vector(x$Sepal.Length), as.vector(d$Sepal.Length), tolerance = 0.001)
     expect_equal(as.vector(x$Petal.Length), as.vector(d$Petal.Length), tolerance = 0.001)
+    expect_equal(colnames(x), c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species", "Sepal.Length_c", "Sepal.Width_c"))
   })
 }
