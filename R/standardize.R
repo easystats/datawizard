6acf81c6cea6e24290b31062d3f2ff7c9571e91e
Daniel <mail@danielluedecke.de>
2021-11-06 10:20:50 +0100

    code style

--- R/standardize.R
+++ R/standardize.R
@@ -250,7 +250,8 @@ standardize.grouped_df <- function(x,
                                    scale = NULL,
                                    ...) {
 
-  args <- .process_grouped_df(x, select, exclude, append, append_suffix = "_z", reference, weights, force)
+  args <- .process_grouped_df(x, select, exclude, append, append_suffix = "_z",
+                              reference, weights, force)
 
   for (rows in args$grps) {
     args$x[rows, ] <- standardize(
