b812df1739a375f1cea47ff22658b667cce9d699
Daniel <mail@danielluedecke.de>
2022-02-12 12:36:08 +0100

    init data_merge

---
+++ R/data_merge.R
@@ -0,0 +1,30 @@
+#' @title Merge (join) two data frames, or a list of data frames
+#' @name data_merge
+#'
+#' @description
+#' Merge (join) two data frames, or a list of data frames.
+#'
+#' @param x,y A data frame to merge
+#' @param join Character vector, indicating the way of joining the data frames.
+#' @param by Specifications of the columns used for merging.
+#' @param id Optional name for ID column that will be created to indicate the source data frames for appended rows.
+#' @param ... Not used.
+#'
+#' @return
+#' A merged data frame.
+#'
+#' @examples
+#' data(iris)
+#' @export
+data_merge <- function(x, ...) {
+  UseMethod("data_merge")
+}
+
+#' @export
+data_join <- data_merge
+
+#' @rdname data_merge
+#' @export
+data_merge.data.frame <- function(x, y, join = "left", by = NULL, id = NULL, ...) {
+
+}
