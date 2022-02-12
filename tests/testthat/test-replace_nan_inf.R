d9afed9cb07628c4d9550fc14d7f21fc8de95e79
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-02-12 11:04:02 +0100

    add `replace_nan_inf()`

---
+++ tests/testthat/test-replace_nan_inf.R
@@ -0,0 +1,26 @@
+test_that("extract from data frame", {
+  x <- c(1, 2, NA, 3, NaN, 4, NA, 5, Inf, -Inf, 6, 7)
+
+  expect_equal(
+    replace_nan_inf(x),
+    c(1, 2, NA, 3, NA, 4, NA, 5, NA, NA, 6, 7)
+  )
+
+  # a dataframe
+  df <- data.frame(
+    x = c(1, NA, 5, Inf, 2, NA),
+    y = c(3, NaN, 4, -Inf, 6, 7),
+    stringsAsFactors = FALSE
+  )
+
+  expect_equal(
+    replace_nan_inf(df),
+    structure(list(
+      x = c(1, NA, 5, NA, 2, NA),
+      y = c(3, NA, 4, NA, 6, 7)
+    ),
+    row.names = c(NA, -6L),
+    class = "data.frame"
+    )
+  )
+})
