9b8f2f93f0e42fe7b0b30eda0e20ab45b1f102af
Daniel <mail@danielluedecke.de>
2022-02-14 20:07:44 +0100

    WIP

--- R/data_recode.R
+++ R/data_recode.R
@@ -1,4 +1,21 @@
-data_recode <- function(x, split = "median", n_groups = NULL) {
+#' @title Recode data
+#' @name data_recode
+#'
+#' @description
+#' Recode data.
+#'
+#' @param x A data frame or vector.
+#' @param split Name of a function, or numeric values, indicating cutoffs
+#' @param n_groups If `split = "quantile"`.
+#'
+#' @export
+data_recode <- function(x, ...) {
+  UseMethod("data_recode")
+}
+
+#' @rdname data_recode
+#' @export
+data_recode.numeric <- function(x, split = "median", n_groups = NULL) {
   # evaluate split-function
   split <- substitute(split)
 
@@ -17,9 +34,7 @@ data_recode <- function(x, split = "median", n_groups = NULL) {
   }
 
   if (is.numeric(split)) {
-    split <- unique(c(min(x), split, max(x)))
-    out <- cut(x, breaks = split)
-    levels(out) <- 1:nlevels(out)
+    cutoffs <- unique(c(min(x), split, max(x)))
   } else {
     cutoffs <- switch(
       split,
@@ -27,10 +42,13 @@ data_recode <- function(x, split = "median", n_groups = NULL) {
       "mean" = mean(x),
       "quantile" = stats::quantile(x, probs = length(x) / (rev(seq(1:n_groups)) * length(x))),
     )
-
   }
 
+  # complete ranges, including minimum and maximum
+  cutoffs <- c(min(x), cutoffs, max(x))
+
+  out <- cut(x, breaks = cutoffs)
+  levels(out) <- 1:nlevels(out)
+
+  out
 }
-
-a <- rev(seq(1:3))
-
