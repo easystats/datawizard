1a16b55287a3df53d659f3829e58755eee842705
etiennebacher <etienne.bacher@protonmail.com>
2022-03-03 19:30:44 +0100

    add rownames_as_column and column_as_rownames

--- R/utils_data.R
+++ R/utils_data.R
@@ -7,10 +7,50 @@
   x
 }
 
+#' Tools for working with row names
+#'
+#' @param x A data frame.
+#' @param var Name of column to use for rownames.
+#'
+#' @return \code{rownames_as_column} and \code{column_as_rownames} both return a data frame.
+#' @export
+#'
+#' @rdname rownames
+#'
+#' @examples
+#' # Convert between row names and column --------------------------------
+#' test <- rownames_as_column(mtcars, var = "car")
+#' test
+#' head(column_as_rownames(test, var = "car"))
 rownames_as_column <- function(x, var = "rowname") {
+  if (is.null(var)) {
+    var <- "rowname"
+  }
+  if (!is.character(var)) {
+    stop("Argument 'var' must be a character.")
+  }
   rn <- data.frame(rn = rownames(x), stringsAsFactors = FALSE)
   x <- cbind(rn, x)
   colnames(x)[1] <- var
   rownames(x) <- NULL
   x
 }
+
+#' @rdname rownames
+#' @export
+#' @examples
+column_as_rownames <- function(x, var = "rowname") {
+  if (is.null(var)) {
+    var <- "rowname"
+  }
+  if (!is.character(var)) {
+    stop("Argument 'var' must be a character.")
+  }
+  if (!var %in% names(x)) {
+    stop(paste0('Variable "', var, '" is not in the dataframe.'))
+  }
+  rownames(x) <- x[[var]]
+  x[[var]] <- NULL
+  x
+}
+
---
+++ tests/testthat/test-utils_data.R
@@ -0,0 +1,45 @@
+test_that("rownames_as_column works", {
+  test <- rownames_as_column(mtcars, "new_column")
+  expect_true("new_column" %in% names(test))
+  expect_true(test[1, "new_column"] == "Mazda RX4")
+})
+
+test_that("rownames_as_column doesn't work if var is not a character", {
+  expect_error(rownames_as_column(mtcars, var = 1))
+  expect_error(rownames_as_column(mtcars, var = TRUE))
+})
+
+test_that("rownames_as_column uses 'rowname' as default column name", {
+  test <- rownames_as_column(mtcars, var = NULL)
+  expect_true("rowname" %in% names(test))
+})
+
+#-------------------------------------------------s
+
+test_that("column_as_rownames works", {
+  continents <- c("Africa", "Asia", "Europe", "North America", "Oceania", "South America")
+  test <- data.frame(
+    continent = continents,
+    some_value = seq(1, 6, by = 1)
+  )
+  test2 <- column_as_rownames(test, "continent")
+  expect_identical(rownames(test2), continents)
+  expect_true(ncol(test2) == 1)
+})
+
+test_that("column_as_rownames doesn't work if column doesn't exist", {
+  continents <- c("Africa", "Asia", "Europe", "North America", "Oceania", "South America")
+  test <- data.frame(
+    continent = continents,
+    some_value = seq(1, 6, by = 1)
+  )
+  expect_error(column_as_rownames(test, "foo"))
+  expect_error(column_as_rownames(test, 3))
+})
+
+
+test_that("rownames_as_column and column_as_rownames cancel each other", {
+  test <- rownames_as_column(mtcars)
+  test2 <- column_as_rownames(test)
+  expect_identical(test2, mtcars)
+})
