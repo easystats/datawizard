ba2250d094f818090eb74cdee522c9462d910692
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-03 11:47:07 +0100

    remove copies of insight functions
    closes #78

---
+++ R/reexports.R
@@ -0,0 +1,19 @@
+#' @export
+#' @importFrom insight is_empty_object
+insight::is_empty_object
+
+#' @export
+#' @importFrom insight object_has_names
+insight::object_has_names
+
+#' @export
+#' @importFrom insight object_has_rownames
+insight::object_has_rownames
+
+#' @export
+#' @importFrom insight compact_list
+insight::compact_list
+
+#' @export
+#' @importFrom insight compact_character
+insight::compact_character
