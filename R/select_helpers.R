dd222f77108040a9311b34bedec197aba2af4d04
Daniel <mail@danielluedecke.de>
2022-03-05 23:46:09 +0100

    forgot col_contains alias

--- R/select_helpers.R
+++ R/select_helpers.R
@@ -11,6 +11,8 @@
     pattern <- paste0("^", gsub("col_starts_with\\(\"(.*)\"\\)", "\\1", x))
   } else if (grepl("^col_ends_with\\(\"(.*)\"\\)", x)) {
     pattern <- paste0(gsub("^ends_with\\(\"(.*)\"\\)", "\\1", x), "$")
+  } else if (grepl("^col_contains\\(\"(.*)\"\\)", x)) {
+    pattern <- paste0("\\Q", gsub("col_contains\\(\"(.*)\"\\)", "\\1", x), "\\E")
   } else if (grepl("^col_matches\\(\"(.*)\"\\)", x)) {
     pattern <- gsub("col_matches\\(\"(.*)\"\\)", "\\1", x)
   } else {
