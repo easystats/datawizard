924fd5a0869fe2a129db87ca24fb6789619b888b
Daniel <mail@danielluedecke.de>
2022-03-05 15:46:51 +0100

    add fixed argument

--- R/data_remove.R
+++ R/data_remove.R
@@ -1,10 +1,12 @@
+#' @param fixed Logical, if `TRUE`, `pattern` is treated as a string to be
+#'   matched as is, else `pattern` is treated as regular expression.
 #' @rdname data_relocate
 #' @examples
 #' # Remove columns
 #' head(data_remove(iris, "Sepal.Length"))
 #' @export
-data_remove <- function(data, pattern, ...) {
-  new <- data[!grepl(pattern, names(data))]
+data_remove <- function(data, pattern, fixed = FALSE, ...) {
+  new <- data[!grepl(pattern, names(data), fixed = fixed)]
   attributes(new) <- utils::modifyList(attributes(data), attributes(new))
   class(new) <- class(data)
   new
