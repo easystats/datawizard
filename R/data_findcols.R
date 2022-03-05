c763c869027cb996d3560b3768e183c38cc0dcbd
Daniel <mail@danielluedecke.de>
2022-03-05 15:36:07 +0100

    typos

--- R/data_findcols.R
+++ R/data_findcols.R
@@ -1,8 +1,8 @@
 #' @param pattern A regular expression (as character string), representing the
 #'   pattern to be matched in the in column names.
-#' @param starts_with,end_with Character string, containing the string to be
+#' @param starts_with,ends_with Character string, containing the string to be
 #'   matched in the column names. `starts_with` finds matches at the beginning
-#'   of column names, `end_with` finds matches at the end of column names.
+#'   of column names, `ends_with` finds matches at the end of column names.
 #' @rdname data_relocate
 #' @examples
 #' # Find columns names by pattern
