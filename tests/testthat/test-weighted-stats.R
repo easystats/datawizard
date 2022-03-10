47e14b00b5e9035bf8f9c0ef2c2b01772d1f8647
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-10 20:44:29 +0100

    activate tests

--- tests/testthat/test-weighted-stats.R
+++ tests/testthat/test-weighted-stats.R
@@ -1,17 +1,10 @@
-# data(efc)
-# set.seed(123)
-# efc$weight <- abs(rnorm(nrow(efc), 1, .3))
-#
-# test_that("weighted centrality and dispersion measures work as expected", {
-#   expect_equal(weighted_sd(efc$c12hour, weights = efc$weight), 51.18224, tolerance = 1e-5)
-#   expect_equal(weighted_sd(efc$c12hour, weights = NULL), 50.80504, tolerance = 1e-5)
-#
-#   expect_equal(weighted_mean(efc$c12hour, weights = efc$weight), 42.80723, tolerance = 1e-5)
-#   expect_equal(weighted_mean(efc$c12hour, weights = NULL), 42.39911, tolerance = 1e-5)
-#
-#   expect_equal(weighted_se(efc$c12hour, weights = efc$weight), 1.704182, tolerance = 1e-5)
-#   expect_equal(weighted_se(efc$c12hour, weights = NULL), 1.691623, tolerance = 1e-5)
-#
-#   expect_equal(weighted_median(efc$c12hour, weights = efc$weight), 20, tolerance = 1e-5)
-#   expect_equal(weighted_median(efc$c12hour, weights = NULL), 20, tolerance = 1e-5)
-# })
+test_that("weighted centrality and dispersion measures work as expected", {
+  x <- c(3.7, 3.3, 3.5, 2.8)
+  wt <- c(5, 5, 4, 1) / 15
+
+  set.seed(123)
+  expect_equal(weighted_mean(x, wt), 3.453333, tolerance = 0.001)
+  expect_equal(weighted_median(x, wt), 3.653333, tolerance = 0.001)
+  expect_equal(weighted_sd(x, wt), 0.2852935, tolerance = 0.001)
+  expect_equal(weighted_mad(x, wt), 0.1594619, tolerance = 0.001)
+})
