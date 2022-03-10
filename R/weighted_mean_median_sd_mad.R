116fd757f790bf81181e830af838836957de7564
Mattan S. Ben-Shachar <35330040+mattansb@users.noreply.github.com>
2022-03-10 16:05:15 +0200

    new weighted_mean (#100)

---
+++ R/weighted_mean_median_sd_mad.R
@@ -0,0 +1,99 @@
+#' Weighted Mean, Median, SD, and MAD
+#'
+#' @inheritParams stats::weighted.mean
+#' @inheritParams stats::mad
+#' @param verbose Show warning when `w` are negative?
+#'
+#' If `w = NULL`, `x` is passed to the non-weighted function.
+#'
+#' @examples
+#' ## GPA from Siegel 1994
+#' x <- c(3.7, 3.3, 3.5, 2.8)
+#' wt <- c(5,  5,  4,  1)/15
+#'
+#' weighted_mean(x, wt)
+#' weighted_median(x, wt)
+#'
+#' weighted_sd(x, wt)
+#' weighted_mad(x, wt)
+#'
+#' @export
+weighted_mean <- function(x, w = NULL, verbose = TRUE, ...) {
+  if (!.are_weights(w) || !.validate_weights(w, verbose)) {
+    return(mean(x, na.rm = TRUE))
+  }
+
+  stats::weighted.mean(x, w, na.rm = TRUE)
+}
+
+
+#' @export
+#' @rdname weighted_mean
+weighted_median <- function(x, w = NULL, verbose = TRUE, ...) {
+  # From spatstat + wiki
+  if (!.are_weights(w) || !.validate_weights(w, verbose)) {
+    return(stats::median(x, na.rm = TRUE))
+  }
+
+  oo <- order(x)
+  x <- x[oo]
+  w <- w[oo]
+  Fx <- cumsum(w) / sum(w)
+
+  lefties <- which(Fx <= 0.5)
+  left <- max(lefties)
+  if (length(lefties) == 0) {
+    result <- x[1]
+  } else if (left == length(x)) {
+    result <- x[length(x)]
+  } else {
+    result <- x[left]
+
+    if (!(Fx[left - 1] < 0.5 && 1 - Fx[left] < 0.5)) {
+      right <- left + 1
+      y <- x[left] * Fx[left] + x[right] * Fx[right]
+      if (is.finite(y)) result <- y
+    }
+  }
+
+  result
+}
+
+#' @export
+#' @rdname weighted_mean
+weighted_sd <- function(x, w = NULL, verbose = TRUE, ...) {
+  # from cov.wt
+  if (!.are_weights(w) || !.validate_weights(w, verbose)) {
+    return(stats::sd(x, na.rm = TRUE))
+  }
+
+  weights1 <- w / sum(w)
+  center <- sum(weights1 * x)
+  xc <- sqrt(weights1) * (x - center)
+  var <- (t(xc) %*% xc) / (1 - sum(weights1^2))
+  sqrt(as.vector(var))
+}
+
+#' @export
+#' @rdname weighted_mean
+weighted_mad <- function(x, w = NULL, constant = 1.4826, verbose = TRUE, ...) {
+  # From matrixStats
+  if (!.are_weights(w) || !.validate_weights(w, verbose)) {
+    return(stats::mad(x, na.rm = TRUE))
+  }
+
+  center <- weighted_median(x, w = w)
+  x <- abs(x - center)
+  constant * weighted_median(x, w = w)
+}
+
+
+# Utils -------------------------------------------------------------------
+
+.validate_weights <- function(w, verbode = TRUE) {
+  pos <- all(w > 0, na.rm = TRUE)
+  if (isTRUE(!pos) && isTRUE(verbose)) {
+    warning("Some w were negative. Weighting not carried out.", call. = FALSE)
+  }
+  pos
+}
