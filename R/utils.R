00418fbc23285090f32b3049ae7c92b5043164c4
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-15 18:14:26 +0100

    use insight helpers (#108)
    https://github.com/easystats/easystats/issues/220

--- R/utils.R
+++ R/utils.R
@@ -6,10 +6,3 @@
   uniqv[idx]
 }
 
-#' @keywords internal
-.trim <- function(x) gsub("^\\s+|\\s+$", "", x)
-
-#' @keywords internal
-.safe_deparse <- function(string) {
-  paste0(sapply(deparse(string, width.cutoff = 500), .trim, simplify = TRUE), collapse = " ")
-}
