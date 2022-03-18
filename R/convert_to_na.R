ef7698b8252653a2cb513a6f24d11a7efb02dfc5
Daniel <mail@danielluedecke.de>
2022-03-18 21:27:52 +0100

    Update convert_to_na.R

--- R/convert_to_na.R
+++ R/convert_to_na.R
@@ -98,38 +98,8 @@ convert_to_na.character <- convert_to_na.factor
 #' @rdname convert_to_na
 #' @export
 convert_to_na.data.frame <- function(x, na = NULL, select = NULL, exclude = NULL, ignore_case = FALSE, verbose = TRUE, ...) {
-  fixed <- TRUE
-  # avoid conflicts
-  conflicting_packages <- .conflicting_packages("poorman")
-
-  # in case pattern is a variable from another function call...
-  p <- try(eval(select), silent = TRUE)
-  if (inherits(p, c("try-error", "simpleError"))) {
-    p <- substitute(select)
-  }
-
-  # check if pattern is a function like "starts_with()"
-  select <- tryCatch(eval(p), error = function(e) NULL)
-
-  # if select could not be evaluated (because expression "makes no sense")
-  # try to evaluate and find select-helpers. In this case, set fixed = FALSE,
-  # so we can use grepl()
-  if (is.null(select)) {
-    evaluated_pattern <- .evaluate_pattern(insight::safe_deparse(p), x, ignore_case = ignore_case)
-    select <- evaluated_pattern$pattern
-    fixed <- evaluated_pattern$fixed
-  }
-
-  # seems to be no valid column name or index, so try to grep
-  if (isFALSE(fixed)) {
-    select <- colnames(x)[grepl(select, colnames(x), ignore.case = ignore_case)]
-  }
-
-  # load again
-  .attach_packages(conflicting_packages)
-
-  # return valid column names, based on pattern
-  select <- .evaluated_pattern_to_colnames(select, x, ignore_case, verbose, exclude)
+  # evaluate arguments
+  select <- .select_nse(select, x, exclude, ignore_case)
 
   x[select] <- lapply(x[select], convert_to_na, na = na, verbose = FALSE, ...)
   x
