2cb9909c965af9d06ea667d62d6049f981fdfd00
Daniel <mail@danielluedecke.de>
2022-03-18 21:27:45 +0100

    Update data_remove.R

--- R/data_remove.R
+++ R/data_remove.R
@@ -9,39 +9,11 @@ data_remove <- function(data, pattern, ignore_case = FALSE, verbose = FALSE, ...
 
   ## TODO set verbose = TRUE by default in a later update?
 
-  fixed <- TRUE
-  # avoid conflicts
-  conflicting_packages <- .conflicting_packages("poorman")
-
-  # in case pattern is a variable from another function call...
-  p <- try(eval(pattern), silent = TRUE)
-  if (inherits(p, c("try-error", "simpleError"))) {
-    p <- substitute(pattern)
-  }
-
-  # check if pattern is a function like "starts_with()",
-  # then override "fixed" argument
-  pattern <- tryCatch(eval(p), error = function(e) NULL)
-
-  if (is.null(pattern)) {
-    evaluated_pattern <- .evaluate_pattern(insight::safe_deparse(p), data, ignore_case)
-    pattern <- evaluated_pattern$pattern
-    fixed <- evaluated_pattern$fixed
-  }
-
-  # load again
-  .attach_packages(conflicting_packages)
-
-  # seems to be no valid column name or index, so try to grep
-  if (isFALSE(fixed) && length(pattern) == 1) {
-    pattern <- colnames(data)[grepl(pattern, colnames(data), ignore.case = ignore_case)]
-  }
-
-  # return valid column names, based on pattern
-  pattern <- .evaluated_pattern_to_colnames(pattern, data, ignore_case, verbose)
+  # evaluate arguments
+  pattern <- .select_nse(pattern, data, exclude = NULL, ignore_case)
 
   # nothing to remove?
-  if (!length(pattern)) {
+  if (!length(pattern) || is.null(pattern)) {
     return(data)
   }
 
