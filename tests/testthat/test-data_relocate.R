d19a0e1f3b6efc5622e8bbc26244e3f03bd76225
Daniel <mail@danielluedecke.de>
2022-03-19 09:56:26 +0100

    use "select" and select-helpers for data_relocate and data_reorder

--- tests/testthat/test-data_relocate.R
+++ tests/testthat/test-data_relocate.R
@@ -1,35 +1,52 @@
 test_that("data_relocate works as expected", {
   expect_error(
-    data_relocate(iris, cols = "Species", before = 2, after = 3),
+    data_relocate(iris, select = "Species", before = 2, after = 3),
     "You must supply only one of `before` or `after`."
   )
 
   expect_error(
-    data_relocate(iris, cols = "Species", before = 10),
+    data_relocate(iris, select = "Species", before = 10),
     "No valid position defined in 'before'."
   )
 
   expect_error(
-    data_relocate(iris, cols = "Species", after = 10),
+    data_relocate(iris, select = "Species", after = 10),
     "No valid position defined in 'after'."
   )
 
   expect_equal(
-    names(data_relocate(iris, cols = "Species", before = "Sepal.Length")),
+    names(data_relocate(iris, select = "Species", before = "Sepal.Length")),
     c("Species", "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")
   )
   expect_equal(
-    names(data_relocate(iris, cols = "Species", before = "Sepal.Width")),
+    names(data_relocate(iris, select = "Species", before = "Sepal.Width")),
     c("Sepal.Length", "Species", "Sepal.Width", "Petal.Length", "Petal.Width")
   )
 
   expect_equal(
-    names(data_relocate(iris, cols = "Sepal.Width", after = "Species")),
-    names(data_relocate(iris, cols = "Sepal.Width", after = -1))
+    names(data_relocate(iris, select = "Sepal.Width", after = "Species")),
+    names(data_relocate(iris, select = "Sepal.Width", after = -1))
   )
 
   expect_equal(
-    names(data_relocate(iris, cols = c("Species", "Petal.Length"), after = "Sepal.Width")),
-    names(data_relocate(iris, cols = c("Species", "Petal.Length"), after = 2))
+    names(data_relocate(iris, select = c("Species", "Petal.Length"), after = "Sepal.Width")),
+    names(data_relocate(iris, select = c("Species", "Petal.Length"), after = 2))
+  )
+})
+
+
+
+test_that("data_relocate select-helpers", {
+  expect_equal(
+    colnames(data_relocate(iris, select = starts_with("Sepal"), after = 5)),
+    colnames(iris[c(3:5, 1:2)])
+  )
+  expect_equal(
+    colnames(data_relocate(iris, select = 1:2, after = 5)),
+    colnames(iris[c(3:5, 1:2)])
+  )
+  expect_equal(
+    colnames(data_relocate(iris, select = -1)),
+    colnames(iris[c(5, 1:4)])
   )
 })
