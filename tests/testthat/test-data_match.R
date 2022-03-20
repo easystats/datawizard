bdd6ea966e75287da2269446628c66a7bacf5155
Dominique Makowski <dom.mak19@gmail.com>
2022-03-20 17:48:14 +0800

    Update test-data_match.R

--- tests/testthat/test-data_match.R
+++ tests/testthat/test-data_match.R
@@ -6,7 +6,7 @@ test_that("data_match works as expected", {
   expect_equal(unique(df1$vs), 0)
   expect_equal(unique(df1$am), 1)
 
-  matching_rows <- data_match(mtcars, data.frame(vs = 0, am = c(0, 1), return_indices = TRUE))
+  matching_rows <- data_match(mtcars, data.frame(vs = 0, am = c(0, 1)), return_indices = TRUE)
   df2 <- mtcars[matching_rows, ]
   expect_equal(unique(df2$vs), 0)
   expect_equal(unique(df2$am), c(1, 0))
