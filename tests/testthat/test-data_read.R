2d0fa9fbc54a3ba7eb5d205c2653c77829cc7d71
Daniel <mail@danielluedecke.de>
2022-04-14 20:08:21 +0200

    Update test-data_read.R

--- tests/testthat/test-data_read.R
+++ tests/testthat/test-data_read.R
@@ -1,5 +1,7 @@
 skip_if_not_installed("httr")
 
+# SPSS file -----------------------------------
+
 temp_file <- tempfile(fileext = ".sav")
 request <- httr::GET("https://raw.github.com/easystats/circus/master/data/EFC.sav")
 httr::stop_for_status(request)
@@ -10,3 +12,22 @@ test_that("data_read", {
   expect_equal(sum(sapply(d, is.factor)), 15)
   expect_equal(sum(sapply(d, is.numeric)), 11)
 })
+
+unlink(temp_file)
+
+
+
+# zipped SPSS file -----------------------------------
+
+temp_file <- tempfile(fileext = ".zip")
+request <- httr::GET("https://raw.github.com/easystats/circus/master/data/EFC.zip")
+httr::stop_for_status(request)
+writeBin(httr::content(request, type = "raw"), temp_file)
+
+test_that("data_read", {
+  d <- data_read(temp_file)
+  expect_equal(sum(sapply(d, is.factor)), 15)
+  expect_equal(sum(sapply(d, is.numeric)), 11)
+})
+
+unlink(temp_file)
