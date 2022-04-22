8de799188515607eb1db228aa6996873689551e1
Daniel <mail@danielluedecke.de>
2022-04-22 08:44:46 +0200

    docs

--- R/data_rename.R
+++ R/data_rename.R
@@ -43,9 +43,9 @@
 #' - Functions to reorder or remove columns: [data_reorder()], [data_relocate()], [data_remove()]
 #' - Functions to reshape, pivot or rotate dataframes: [data_to_long()], [data_to_wide()], [data_rotate()]
 #' - Functions to recode data: [data_rescale()], [data_reverse()], [data_cut()], [data_recode()], [data_shift()]
-#' - Functions to standardize, normalize, rank-transform: [standardize()], [normalize()], [ranktransform()], [winsorize()]
+#' - Functions to standardize, normalize, rank-transform: [center()], [standardize()], [normalize()], [ranktransform()], [winsorize()]
 #' - Split and merge dataframes: [data_partition()], [data_merge()]
-#' - Functions to find or select columns: [data_select()], [find_columns()]
+#' - Functions to find or select columns: [data_select()], [data_find()]
 #' - Functions to filter rows: [data_match()], [data_filter()]
 #'
 #' @export
