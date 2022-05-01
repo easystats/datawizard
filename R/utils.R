9e2e834d57e88c8add8e833a1e77e7b929bd2cc2
Daniel <mail@danielluedecke.de>
2022-05-01 15:49:39 +0200

    #146 move standardize default to datawizard (#158)
    * move standardize default to datawizard
    
    * add tests
    
    * don't stop
    
    * typo
    
    * Update DESCRIPTION
    
    * Update test-standardize_models.R
    
    * Update NEWS.md

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,11 @@
   idx <- which.max(tab)
   uniqv[idx]
 }
+
+
+#' @keywords internal
+.get_model_info <- function(model, model_info = NULL, ...) {
+  if (is.null(model_info)) model_info <- insight::model_info(model)
+
+  model_info
+}
