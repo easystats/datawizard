a97ff8264ff8e67770b3dab59c8b40144a85d570
Daniel <mail@danielluedecke.de>
2022-06-29 12:14:28 +0200

    Data partition impr2 (#181)
    * initial
    
    * Update data_partition.R
    
    * Update data_partition.R
    
    * Update data_partition.R
    
    * polish
    
    * tests
    
    * typo
    
    * more accessible element names, no rownames
    
    * more input checks
    
    * Update data_partition.R
    
    * Update data_partition.R
    
    * add verbose arg, remove unused code
    
    * Update test-data_partition.R
    
    * update examples
    
    * Update NEWS.md
    
    * comment code
    
    * prob -> proportions
    
    * proportions -> proportion
    
    Co-authored-by: Dominique Makowski <dom.mak19@gmail.com>
    Co-authored-by: Indrajeet Patil <patilindrajeet.science@gmail.com>

--- tests/testthat/test-data_partition.R
+++ tests/testthat/test-data_partition.R
@@ -3,25 +3,52 @@ test_that("data_partition works as expected", {
 
   expect_error(
     data_partition(new.env()),
-    "`data` needs to be a data frame, or an object that can be coerced to a data frame."
+    "`data` needs to be a data frame"
   )
 
   # to be coerced to dataframes
 
   expect_snapshot(data_partition(letters, seed = 123))
 
+  # sanity checks
+
+  expect_warning(
+    data_partition(iris, 0.7, row_id = "Species"),
+    "exists"
+  )
+
+  expect_warning(
+    data_partition(iris, c(0.7, 0.3), row_id = "Species"),
+    "generated"
+  )
+
+  # values
+
+  out <- data_partition(mtcars, proportion = .8, seed = 123)
+
+  expect_equal(
+    out$p_0.8$.row_id,
+    c(1L, 3L, 4L, 5L, 7L, 8L, 9L, 10L, 11L, 14L, 15L, 17L, 18L, 19L,
+      20L, 21L, 22L, 23L, 24L, 26L, 27L, 28L, 29L, 30L, 31L, 32L)
+  )
+
+  expect_equal(
+    colnames(out$p_0.8),
+    c("mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am",
+      "gear", "carb", ".row_id")
+  )
+
+  expect_equal(
+    lapply(out, nrow),
+    list(p_0.8 = 26L, test = 6L)
+  )
+
+
   # dataframes
 
-  df <- iris
-  set.seed(123)
-  df$Smell <- rep(c("Strong", "Light"), 75)
-
-  set.seed(123)
-  expect_snapshot(str(data_partition(df)))
-
-  set.seed(123)
-  expect_snapshot(str(data_partition(df, group = "Species")))
-
-  set.seed(123)
-  expect_snapshot(str(data_partition(df, group = c("Species", "Smell"))))
+  data(iris)
+  expect_snapshot(str(data_partition(iris, proportion = .7, seed = 123)))
+  expect_snapshot(str(data_partition(iris, proportion = c(.2, .5), seed = 123)))
+  expect_snapshot(str(data_partition(iris, proportion = .7, group = "Species", seed = 123)))
+  expect_snapshot(str(data_partition(iris, proportion = c(.2, .5), group = "Species", seed = 123)))
 })
