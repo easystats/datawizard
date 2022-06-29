35590f95327fd26cc77cf18fd15a1c5ef036683e
Daniel <mail@danielluedecke.de>
2022-06-29 11:34:01 +0200

    Update test-data_partition.R

--- tests/testthat/test-data_partition.R
+++ tests/testthat/test-data_partition.R
@@ -17,6 +17,33 @@ test_that("data_partition works as expected", {
     "exists"
   )
 
+  expect_warning(
+    data_partition(iris, c(0.7, 0.3), row_id = "Species"),
+    "generated"
+  )
+
+  # values
+
+  out <- data_partition(mtcars, prob = .8, seed = 123)
+
+  expect_equal(
+    out$p_0.8$.row_id,
+    c(1L, 3L, 4L, 5L, 7L, 8L, 9L, 10L, 11L, 14L, 15L, 17L, 18L, 19L,
+      20L, 21L, 22L, 23L, 24L, 26L, 27L, 28L, 29L, 30L, 31L, 32L)
+  )
+
+  expect_equal(
+    colnames(out$p_0.8),
+    c("mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am",
+      "gear", "carb", ".row_id")
+  )
+
+  expect_equal(
+    lapply(out, nrow),
+    list(p_0.8 = 26L, test = 6L)
+  )
+
+
   # dataframes
 
   data(iris)
