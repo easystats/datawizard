162ba520ae7655d904897562b8f155b90d33d305
Daniel <mail@danielluedecke.de>
2022-08-29 12:52:13 +0200

    typo

--- tests/testthat/test-data_match.R
+++ tests/testthat/test-data_match.R
@@ -81,7 +81,7 @@ test_that("data_filter works like slice", {
 
 test_that("data_filter gives informative message on errors", {
   expect_error(
-    data_filter(mtcars, "filter = mpg > 10 || cyl = 4"),
+    data_filter(mtcars, "mpg > 10 || cyl = 4"),
     "`==`"
   )
   expect_error(
