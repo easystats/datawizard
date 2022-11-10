4e029fa68af56b67fc1ee7c87b248151b5913de4
rempsyc <remi.theriault@mail.mcgill.ca>
2022-11-10 11:59:36 -0500

    Add all args for selection (exclude, regex, etc.)

--- R/data_duplicated.R
+++ R/data_duplicated.R
@@ -8,7 +8,51 @@
 #' selecting which duplicates to keep.
 #'
 #' @param data The data frame.
-#' @param select The id variable for which to check for duplicates.
+#' @param select Variables that will be included when performing the required
+#'   tasks. Can be either
+#'
+#'   - a variable specified as a literal variable name (e.g., `column_name`),
+#'   - a string with the variable name (e.g., `"column_name"`), or a character
+#'     vector of variable names (e.g., `c("col1", "col2", "col3")`),
+#'   - a formula with variable names (e.g., `~column_1 + column_2`),
+#'   - a vector of positive integers, giving the positions counting from the left
+#'     (e.g. `1` or `c(1, 3, 5)`),
+#'   - a vector of negative integers, giving the positions counting from the
+#'     right (e.g., `-1` or `-1:-3`),
+#'   - one of the following select-helpers: `starts_with()`, `ends_with()`,
+#'     `contains()`, a range using `:` or `regex("")`. `starts_with()`,
+#'     `ends_with()`, and  `contains()` accept several patterns, e.g
+#'     `starts_with("Sep", "Petal")`.
+#'   - or a function testing for logical conditions, e.g. `is.numeric()` (or
+#'     `is.numeric`), or any user-defined function that selects the variables
+#'     for which the function returns `TRUE` (like: `foo <- function(x) mean(x) > 3`),
+#'   - ranges specified via literal variable names, select-helpers (except
+#'     `regex()`) and (user-defined) functions can be negated, i.e. return
+#'     non-matching elements, when prefixed with a `-`, e.g. `-ends_with("")`,
+#'     `-is.numeric` or `-Sepal.Width:Petal.Length`. **Note:** Negation means
+#'     that matches are _excluded_, and thus, the `exclude` argument can be
+#'     used alternatively. For instance, `select=-ends_with("Length")` (with
+#'     `-`) is equivalent to `exclude=ends_with("Length")` (no `-`). In case
+#'     negation should not work as expected, use the `exclude` argument instead.
+#'
+#'   If `NULL`, selects all columns. Patterns that found no matches are silently
+#'   ignored, e.g. `find_columns(iris, select = c("Species", "Test"))` will just
+#'   return `"Species"`.
+#' @param exclude See `select`, however, column names matched by the pattern
+#'   from `exclude` will be excluded instead of selected. If `NULL` (the default),
+#'   excludes no columns.
+#' @param ignore_case Logical, if `TRUE` and when one of the select-helpers or
+#'   a regular expression is used in `select`, ignores lower/upper case in the
+#'   search pattern when matching against variable names.
+#' @param regex Logical, if `TRUE`, the search pattern from `select` will be
+#'   treated as regular expression. When `regex = TRUE`, select *must* be a
+#'   character string (or a variable containing a character string) and is not
+#'   allowed to be one of the supported select-helpers or a character vector
+#'   of length > 1. `regex = TRUE` is comparable to using one of the two
+#'   select-helpers, `select = contains("")` or `select = regex("")`, however,
+#'   since the select-helpers may not work when called from inside other
+#'   functions (see 'Details'), this argument may be used as workaround.
+#' @param verbose Toggle messages and warnings.
 #' @keywords duplicates
 #' @export
 #' @seealso
@@ -31,17 +75,30 @@
 #' df2 <- df1[-c(1, 5),]
 #' df2
 
-data_duplicated <- function(data, select = NULL) {
+data_duplicated <- function(data,
+                            select = NULL,
+                            exclude = NULL,
+                            ignore_case = FALSE,
+                            regex = FALSE,
+                            verbose = TRUE) {
   UseMethod("data_duplicated")
 }
 
 #' @export
-data_duplicated.data.frame <- function(data, select) {
+data_duplicated.data.frame <- function(data,
+                                       select = NULL,
+                                       exclude = NULL,
+                                       ignore_case = FALSE,
+                                       regex = FALSE,
+                                       verbose = TRUE) {
 
-  select <- .select_nse(select,
+  select <- .select_nse(
+    select,
     data,
-    exclude = NULL,
-    ignore_case = TRUE
+    exclude = exclude,
+    ignore_case = ignore_case,
+    regex = regex,
+    verbose = verbose
   )
 
   data$temporary_id <- do.call(paste, c(data_select(data, select), sep = "_"))
@@ -57,12 +114,19 @@ data_duplicated.data.frame <- function(data, select) {
 }
 
 #' @export
-data_duplicated.grouped_df <- function(data, select = NULL) {
+data_duplicated.grouped_df <- function(data,
+                                       select = NULL,
+                                       exclude = NULL,
+                                       ignore_case = FALSE,
+                                       regex = FALSE,
+                                       verbose = TRUE) {
 
   select <- .select_nse(select,
                         data,
-                        exclude = NULL,
-                        ignore_case = TRUE
+                        exclude = exclude,
+                        ignore_case = ignore_case,
+                        regex = regex,
+                        verbose = verbose
   )
 
   # works only for dplyr >= 0.8.0
--- R/data_unique.R
+++ R/data_unique.R
@@ -9,9 +9,52 @@
 #' authentic, given practice effects.
 #'
 #' @param data The data frame.
-#' @param select The ID variable(s) for which to check for duplicates.
-#' @param keep The method to be used for duplicate selection,
-#' either "best" (the default), "first", or "last".
+#' @param select Variables that will be included when performing the required
+#'   tasks. Can be either
+#'
+#'   - a variable specified as a literal variable name (e.g., `column_name`),
+#'   - a string with the variable name (e.g., `"column_name"`), or a character
+#'     vector of variable names (e.g., `c("col1", "col2", "col3")`),
+#'   - a formula with variable names (e.g., `~column_1 + column_2`),
+#'   - a vector of positive integers, giving the positions counting from the left
+#'     (e.g. `1` or `c(1, 3, 5)`),
+#'   - a vector of negative integers, giving the positions counting from the
+#'     right (e.g., `-1` or `-1:-3`),
+#'   - one of the following select-helpers: `starts_with()`, `ends_with()`,
+#'     `contains()`, a range using `:` or `regex("")`. `starts_with()`,
+#'     `ends_with()`, and  `contains()` accept several patterns, e.g
+#'     `starts_with("Sep", "Petal")`.
+#'   - or a function testing for logical conditions, e.g. `is.numeric()` (or
+#'     `is.numeric`), or any user-defined function that selects the variables
+#'     for which the function returns `TRUE` (like: `foo <- function(x) mean(x) > 3`),
+#'   - ranges specified via literal variable names, select-helpers (except
+#'     `regex()`) and (user-defined) functions can be negated, i.e. return
+#'     non-matching elements, when prefixed with a `-`, e.g. `-ends_with("")`,
+#'     `-is.numeric` or `-Sepal.Width:Petal.Length`. **Note:** Negation means
+#'     that matches are _excluded_, and thus, the `exclude` argument can be
+#'     used alternatively. For instance, `select=-ends_with("Length")` (with
+#'     `-`) is equivalent to `exclude=ends_with("Length")` (no `-`). In case
+#'     negation should not work as expected, use the `exclude` argument instead.
+#'
+#'   If `NULL`, selects all columns. Patterns that found no matches are silently
+#'   ignored, e.g. `find_columns(iris, select = c("Species", "Test"))` will just
+#'   return `"Species"`.
+#' @param keep The method to be used for duplicate selection, either "best"
+#'   (the default), "first", or "last".
+#' @param exclude See `select`, however, column names matched by the pattern
+#'   from `exclude` will be excluded instead of selected. If `NULL` (the default),
+#'   excludes no columns.
+#' @param ignore_case Logical, if `TRUE` and when one of the select-helpers or
+#'   a regular expression is used in `select`, ignores lower/upper case in the
+#'   search pattern when matching against variable names.
+#' @param regex Logical, if `TRUE`, the search pattern from `select` will be
+#'   treated as regular expression. When `regex = TRUE`, select *must* be a
+#'   character string (or a variable containing a character string) and is not
+#'   allowed to be one of the supported select-helpers or a character vector
+#'   of length > 1. `regex = TRUE` is comparable to using one of the two
+#'   select-helpers, `select = contains("")` or `select = regex("")`, however,
+#'   since the select-helpers may not work when called from inside other
+#'   functions (see 'Details'), this argument may be used as workaround.
 #' @param verbose Toggle messages and warnings.
 #' @return A dataframe, containing only the chosen duplicates.
 #' @seealso
@@ -27,18 +70,33 @@
 #'
 #' data_unique(df1, select = "id")
 
-data_unique <- function(data, select = NULL, keep = "best", verbose = TRUE) {
+data_unique <- function(data,
+                        select = NULL,
+                        keep = "best",
+                        exclude = NULL,
+                        ignore_case = FALSE,
+                        regex = FALSE,
+                        verbose = TRUE) {
   UseMethod("data_unique")
 }
 
 
 #' @export
-data_unique.data.frame <- function(data, select, keep = "best", verbose = TRUE) {
+data_unique.data.frame <- function(data,
+                                   select = NULL,
+                                   keep = "best",
+                                   exclude = NULL,
+                                   ignore_case = FALSE,
+                                   regex = FALSE,
+                                   verbose = TRUE) {
 
-  select <- .select_nse(select,
+  select <- .select_nse(
+    select,
     data,
-    exclude = NULL,
-    ignore_case = TRUE
+    exclude = exclude,
+    ignore_case = ignore_case,
+    regex = regex,
+    verbose = verbose
   )
 
   # temporary_id <- paste0(sample(letters), collapse = "")
@@ -84,14 +142,22 @@ data_unique.data.frame <- function(data, select, keep = "best", verbose = TRUE) 
 }
 
 
-
 #' @export
-data_unique.grouped_df <- function(data, select = NULL, keep = "best", verbose = TRUE) {
+data_unique.grouped_df <- function(data,
+                                   select = NULL,
+                                   keep = "best",
+                                   exclude = NULL,
+                                   ignore_case = FALSE,
+                                   regex = FALSE,
+                                   verbose = TRUE) {
 
-  select <- .select_nse(select,
+  select <- .select_nse(
+    select,
     data,
-    exclude = NULL,
-    ignore_case = TRUE
+    exclude = exclude,
+    ignore_case = ignore_case,
+    regex = regex,
+    verbose = verbose
   )
 
   # works only for dplyr >= 0.8.0
