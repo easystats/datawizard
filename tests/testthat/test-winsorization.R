88ded39ca9eb17cb7fa9e7e83fc69eee55e5313b
etiennebacher <etienne.bacher@protonmail.com>
2023-01-24 22:07:02 +0100

    `winsorize`: change warnings to errors

--- tests/testthat/test-winsorization.R
+++ tests/testthat/test-winsorization.R
@@ -10,33 +10,26 @@ test_that("with missing values", {
 })
 
 test_that("winsorize: threshold must be between 0 and 1", {
-  expect_warning(
+  expect_error(
     winsorize(sample(1:10, 5), threshold = -0.1),
     regexp = "must be a scalar between 0 and 0.5"
   )
-  expect_warning(
+  expect_error(
     winsorize(sample(1:10, 5), threshold = 1.1),
     regexp = "must be a scalar between 0 and 0.5"
   )
-  expect_warning(
+  expect_error(
     winsorize(sample(1:10, 5), method = "zscore", threshold = -3),
     regexp = "must be a scalar greater than 0"
   )
-  expect_warning(
+  expect_error(
     winsorize(sample(1:10, 5), method = "zscore", threshold = -3, robust = TRUE),
     regexp = "must be a scalar greater than 0"
   )
-  expect_warning(
+  expect_error(
     winsorize(sample(1:10, 5), method = "raw", threshold = 1.1),
     regexp = "must be of length 2 for lower and upper bound"
   )
-  x <- sample(1:10, 5)
-  suppressWarnings({
-    y <- winsorize(x, threshold = -0.1)
-    z <- winsorize(x, threshold = 1.1)
-  })
-  expect_equal(x, y)
-  expect_equal(x, z)
 })
 
 test_that("winsorize on data.frame", {
