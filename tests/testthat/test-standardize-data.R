fa780e04c5aa6daa169f973cfb21e560ce8ca010
Daniel <mail@danielluedecke.de>
2023-03-02 22:37:48 +0100

    fix lintr

--- tests/testthat/test-standardize-data.R
+++ tests/testthat/test-standardize-data.R
@@ -22,11 +22,11 @@ test_that("standardize.numeric", {
 # standardize factor / Date -----------------------------------------------
 test_that("standardize.numeric", {
   f <- factor(c("c", "a", "b"))
-  expect_equal(standardize(f), f)
+  expect_identical(standardize(f), f)
   expect_equal(standardize(f, force = TRUE), c(1, -1, 0), ignore_attr = TRUE)
 
   d <- as.Date(c("1989/08/06", "1989/08/04", "1989/08/05"))
-  expect_equal(standardize(d), d)
+  expect_identical(standardize(d), d)
   expect_equal(standardize(d, force = TRUE), c(1, -1, 0), ignore_attr = TRUE)
 })
 
@@ -63,18 +63,18 @@ test_that("standardize.data.frame, NAs", {
   x <- standardize(iris)
   expect_equal(head(x$Sepal.Length), c(-0.9163, -1.1588, -1.4013, -1.5226, -1.0376, NA), tolerance = 0.01)
   expect_equal(head(x$Sepal.Width), c(1.0237, -0.151, 0.3189, 0.0839, 1.2586, 1.9635), tolerance = 0.01)
-  expect_equal(mean(x$Sepal.Length), NA_real_)
+  expect_identical(mean(x$Sepal.Length), NA_real_)
 
   x <- standardize(iris, two_sd = TRUE)
   expect_equal(head(x$Sepal.Length), c(-0.4603, -0.5811, -0.7019, -0.7623, -0.5207, NA), tolerance = 0.01)
   expect_equal(head(x$Sepal.Width), c(0.5118, -0.0755, 0.1594, 0.042, 0.6293, 0.9817), tolerance = 0.01)
-  expect_equal(mean(x$Sepal.Length), NA_real_)
+  expect_identical(mean(x$Sepal.Length), NA_real_)
 
 
   x <- standardize(poorman::group_by(iris, .data$Species))
   expect_equal(head(x$Sepal.Length), c(0.2547, -0.3057, -0.8661, -1.1463, -0.0255, NA), tolerance = 0.01)
   expect_equal(head(x$Sepal.Width), c(0.2369, -1.0887, -0.5584, -0.8235, 0.502, 1.2974), tolerance = 0.01)
-  expect_equal(mean(x$Sepal.Length), NA_real_)
+  expect_identical(mean(x$Sepal.Length), NA_real_)
 })
 
 
@@ -93,18 +93,18 @@ test_that("standardize.data.frame, apend", {
   ))
   expect_equal(head(x$Sepal.Length_z), c(-0.8953, -1.1385, -1.3816, -1.5032, -1.0169, -0.5306), tolerance = 0.01)
   expect_equal(head(x$Sepal.Width_z), c(1.04, -0.1029, 0.3543, 0.1257, 1.2685, 1.9542), tolerance = 0.01)
-  expect_equal(mean(x$Sepal.Length_z), NA_real_)
+  expect_identical(mean(x$Sepal.Length_z), NA_real_)
 
   x <- standardize(iris, two_sd = TRUE, append = TRUE)
   expect_equal(head(x$Sepal.Length_z), c(-0.4477, -0.5692, -0.6908, -0.7516, -0.5084, -0.2653), tolerance = 0.01)
   expect_equal(head(x$Sepal.Width_z), c(0.52, -0.0514, 0.1771, 0.0629, 0.6343, 0.9771), tolerance = 0.01)
-  expect_equal(mean(x$Sepal.Length_z), NA_real_)
+  expect_identical(mean(x$Sepal.Length_z), NA_real_)
 
 
   x <- standardize(poorman::group_by(iris, .data$Species), append = TRUE)
   expect_equal(head(x$Sepal.Length_z), c(0.2746, -0.2868, -0.8483, -1.129, -0.0061, 1.1168), tolerance = 0.01)
   expect_equal(head(x$Sepal.Width_z), c(0.1766, -1.1051, -0.5924, -0.8487, 0.4329, 1.2019), tolerance = 0.01)
-  expect_equal(mean(x$Sepal.Length_z), NA_real_)
+  expect_identical(mean(x$Sepal.Length_z), NA_real_)
 })
 
 
@@ -114,17 +114,17 @@ test_that("standardize.data.frame, weights", {
   x <- rexp(30)
   w <- rpois(30, 20) + 1
 
-  expect_equal(
+  expect_identical(
     sqrt(cov.wt(cbind(x, x), w)$cov[1, 1]),
     attr(standardize(x, weights = w), "scale")
   )
-  expect_equal(
+  expect_identical(
     standardize(x, weights = w),
     standardize(data.frame(x), weights = w)$x
   )
 
   # name and vector give same results
-  expect_equal(
+  expect_identical(
     standardize(mtcars, exclude = "cyl", weights = mtcars$cyl),
     standardize(mtcars, weights = "cyl")
   )
@@ -230,7 +230,7 @@ test_that("unstandardize does nothing with characters and factors", {
     unstandardise(c("a", "b")),
     c("a", "b")
   )
-  expect_equal(
+  expect_identical(
     unstandardise(factor(c(1, 2))),
     factor(c(1, 2))
   )
@@ -238,7 +238,7 @@ test_that("unstandardize does nothing with characters and factors", {
 
 # select helpers ------------------------------
 test_that("standardize regex", {
-  expect_equal(
+  expect_identical(
     standardize(mtcars, select = "pg", regex = TRUE),
     standardize(mtcars, select = "mpg")
   )
