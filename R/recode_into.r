72681bcfd133e49d14ef29d14ed87f7562519a54
Daniel <mail@danielluedecke.de>
2023-03-07 18:40:37 +0100

    address comments

--- R/recode_into.r
+++ R/recode_into.r
@@ -3,30 +3,22 @@
 #'
 #' @description
 #' This functions recodes values from one or more variables into a new variable.
-#' It is a convenient function to avoid nested [`ifelse()`] statements.
+#' It is a convenient function to avoid nested [`ifelse()`] statements, which
+#' is similar to `dplyr::case_when()`.
 #'
 #' @param ... A sequence of two-sided formulas, where the left hand side (LHS)
 #' is a logical condition that determines which values match this case. The
-#' right hand side (RHS) indicates the replacement value. The last formula can
-#' be used to indicate the default value. If the `default` argument is provided,
-#' the last formula is considered as further recode-pattern.
+#' right hand side (RHS) indicates the replacement value.
 #' @param data Optional, name of a data frame. This can be used to avoid writing
 #' the data name multiple times in `...`. See 'Examples'.
 #' @param default Indicates the default value that is chosen when no match in
-#' the formulas in `...` is found. If `NULL`, the default value needs to be
-#' determined as last formula in `...`. See 'Examples'.
+#' the formulas in `...` is found. If not provided, `NA` is used as default
+#' value.
 #'
 #' @return A vector with recoded values.
 #'
 #' @examples
 #' x <<- 1:30
-#' recode_into(
-#'   x > 15 ~ "a",
-#'   x > 10 & x <= 15 ~ "b",
-#'   ~"c"
-#' )
-#'
-#' # use default argument
 #' recode_into(
 #'   x > 15 ~ "a",
 #'   x > 10 & x <= 15 ~ "b",
@@ -44,7 +36,7 @@
 #' recode_into(
 #'   d$x %in% 1:3 & d$y %in% c("a", "b") ~ 1,
 #'   d$x > 3 ~ 2,
-#'   TRUE ~ 0 # the LHS of this formula is arbitrary and can be omitted
+#'   default = 0
 #' )
 #'
 #' # no need to write name of data frame each time
@@ -66,7 +58,7 @@
 #'     )
 #' }
 #' @export
-recode_into <- function(..., data = NULL, default = NULL) {
+recode_into <- function(..., data = NULL, default = NA, verbose = TRUE) {
   dots <- list(...)
 
   # get length of vector, so we know the length of the output vector
@@ -76,22 +68,36 @@ recode_into <- function(..., data = NULL, default = NULL) {
     length(with(data, eval(dots[[1]][[2]])))
   }
 
-  # how many expressions (recodes) do we have? last is assumed to default
-  n_params <- length(dots) - 1
+  # how many expressions (recode-formulas) do we have?
+  n_params <- length(dots)
 
   # last expression should always be the default value
-  if (!is.null(default)) {
-    default <- default
-    # if last expression is not default, increase n_params again
-    n_params <- n_params + 1
-  } else if (length(dots[[n_params + 1]]) == 3) {
-    default <- dots[[n_params + 1]][[3]]
-  } else {
-    default <- dots[[n_params + 1]][[2]]
+  if (is.null(default)) {
+    default <- NA
+    if (verbose) {
+      insight::format_warning("Default value can't be `NULL`, setting to `NA` now.")
+    }
   }
 
   # create default output vector
   out <- rep(default, times = len)
+
+  # check recode values
+  for (i in seq_len(n_params)) {
+    # get type of all recode values
+    if (is.null(data)) {
+      all_recodes <- typeof(eval(dots[[i]][[3]]))
+    } else {
+      all_recodes <- typeof(with(data, eval(dots[[i]][[3]])))
+    }
+    # if we have mixed types, warn user
+    if (!(all_recodes == all_recodes[1]) && verbose) {
+      insight::format_warning(
+        "Not all recode values are of the same type.",
+        "Trying to coerce all values into a common type, please check your recoded variable!"
+      )
+    }
+  }
 
   # iterate all expressions
   for (i in seq_len(n_params)) {
