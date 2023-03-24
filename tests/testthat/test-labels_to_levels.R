427588ae7a2e7c1352b12aeda204966d908f5a5c
Daniel <mail@danielluedecke.de>
2023-03-24 12:09:34 +0100

    add test for ordered

--- tests/testthat/test-labels_to_levels.R
+++ tests/testthat/test-labels_to_levels.R
@@ -12,6 +12,12 @@ test_that("labels_to_levels, factor", {
   x <- datawizard::labels_to_levels(x)
   expect_identical(levels(x), c("low", "mid", "high"))
   expect_equal(table(x), table(efc$c172code), ignore_attr = TRUE)
+
+  x <- as.ordered(efc$c172code)
+  attr(x, "labels") <- c("low" = 1, "mid" = 2, "high" = 3)
+  x <- datawizard::labels_to_levels(x)
+  expect_identical(levels(x), c("low", "mid", "high"))
+  expect_s3_class(x, "ordered")
 })
 
 test_that("labels_to_levels, factor, error on no labels", {
