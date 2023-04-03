6fb52aec2545725fee212e334da53711530977de
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-04-03 19:52:36 +0200

    poorman needs to be installed

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
-library(testthat)
-library(datawizard)
+# `{poorman}` needs to be installed because the `helper.R` globally defines pipe
+# operator for the test suite, and there is no point proceeding with testing
+# if it's not available
+if (require("testthat", quietly = TRUE) && require("poorman", quietly = TRUE)) {
+  library(testthat)
+  library(datawizard)
 
-test_check("datawizard")
+  test_check("datawizard")
+}
