1043c7472d1c43722664b2c9616b329f1c2c8c2c
Daniel <mail@danielluedecke.de>
2023-09-12 14:08:20 +0200

    source -> search

--- tests/testthat/test-seek_data.R
+++ tests/testthat/test-seek_data.R
@@ -16,7 +16,7 @@ test_that("seek_variables - search label attribute", {
   data(efc)
   out <- seek_variables(efc, "female")
   expect_identical(nrow(out), 0L)
-  out <- seek_variables(efc, "female", source = "all")
+  out <- seek_variables(efc, "female", search = "all")
   expect_identical(out$index, which(colnames(efc) == out$column))
   expect_identical(out$labels, "elder's gender")
 })
@@ -32,8 +32,41 @@ test_that("seek_variables - fuzzy match", {
 
 test_that("seek_variables - fuzzy match, value labels", {
   data(efc)
-  out <- seek_variables(efc, "femlae", source = "all", fuzzy = TRUE)
+  out <- seek_variables(efc, "femlae", search = "all", fuzzy = TRUE)
   expect_identical(nrow(out), 1L)
   expect_identical(out$index, which(colnames(efc) %in% out$column))
   expect_identical(out$labels, "elder's gender")
+})
+
+test_that("seek_variables - multiple pattern", {
+  data(efc)
+  out <- seek_variables(efc, c("e16", "e42"))
+  expect_identical(nrow(out), 2L)
+  expect_identical(out$index, which(colnames(efc) %in% out$column))
+  expect_identical(out$labels, c("elder's gender", "elder's dependency"))
+  # only one match, typo
+  out <- seek_variables(efc, c("femlae", "dependency"))
+  expect_identical(nrow(out), 1L)
+  expect_identical(out$index, which(colnames(efc) %in% out$column))
+  expect_identical(out$labels, "elder's dependency")
+  # only one match, not searching in value labels
+  out <- seek_variables(efc, c("female", "dependency"))
+  expect_identical(nrow(out), 1L)
+  expect_identical(out$index, which(colnames(efc) %in% out$column))
+  expect_identical(out$labels, "elder's dependency")
+  # two matches
+  out <- seek_variables(efc, c("female", "dependency"), search = "all")
+  expect_identical(nrow(out), 2L)
+  expect_identical(out$index, which(colnames(efc) %in% out$column))
+  expect_identical(out$labels, c("elder's gender", "elder's dependency"))
+  # only one match, typo
+  out <- seek_variables(efc, c("femlae", "dependency"), search = "all")
+  expect_identical(nrow(out), 1L)
+  expect_identical(out$index, which(colnames(efc) %in% out$column))
+  expect_identical(out$labels, "elder's dependency")
+  # two matches, despite typo
+  out <- seek_variables(efc, c("femlae", "dependency"), search = "all", fuzzy = TRUE)
+  expect_identical(nrow(out), 2L)
+  expect_identical(out$index, which(colnames(efc) %in% out$column))
+  expect_identical(out$labels, c("elder's gender", "elder's dependency"))
 })
