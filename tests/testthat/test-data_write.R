09ae21372329bb1dff3880ce24bffb2a03194efc
Daniel <mail@danielluedecke.de>
2023-09-14 13:40:16 +0200

    fix data_write for partially labelled characters

--- tests/testthat/test-data_write.R
+++ tests/testthat/test-data_write.R
@@ -132,3 +132,19 @@ test_that("data_write, no file extension", {
   expect_error(data_write(d, "mytestfile"))
   expect_error(data_write(d, NULL))
 })
+
+
+# writing character vector works for missing value labels ------------------
+
+tmp <- tempfile(fileext = ".sav")
+on.exit(unlink(tmp))
+
+test_that("data_write, no file extension", {
+  d <- data.frame(
+    a = letters[1:3],
+    stringsAsFactors = FALSE
+  )
+  d$a <- assign_labels(d$a, variable = "First")
+  # expect message, but no error
+  expect_message(data_write(d, "test.sav"), regex = "Preparing")
+})
