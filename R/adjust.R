aa3546268bdb8b219be01b142d190ed2f7dce429
Daniel <mail@danielluedecke.de>
2024-12-13 16:42:56 +0100

    fix

--- R/adjust.R
+++ R/adjust.R
@@ -43,7 +43,7 @@
 #' }
 #'
 #' # Generate data
-#' data <- simulate_correlation(n = 100, r = 0.7)
+#' data <- bayestestR::simulate_correlation(n = 100, r = 0.7)
 #' data$V2 <- (5 * data$V2) + 20 # Add intercept
 #'
 #' # Adjust
