224d9531db67c17e3ce07c55464f993142cbb692
Daniel <mail@danielluedecke.de>
2024-12-23 11:32:25 +0100

    Fix `data_tabulate()` for multiple `by` in HTML

--- R/data_xtabulate.R
+++ R/data_xtabulate.R
@@ -90,18 +90,24 @@
 
 
 #' @export
-format.datawizard_crosstab <- function(x, format = "text", digits = 1, big_mark = NULL, ...) {
+format.datawizard_crosstab <- function(x,
+                                       format = "text",
+                                       digits = 1,
+                                       big_mark = NULL,
+                                       include_total_row = TRUE,
+                                       ...) {
   # convert to character manually, else, for large numbers,
   # format_table() returns scientific notation
   x <- as.data.frame(x)
 
   # remove group variable
   x$Group <- NULL
+  numeric_columns <- vapply(x, is.numeric, logical(1))
 
-  # compute total N for rows and colummns
+  # compute total N for rows and columns
   total_n <- attributes(x)$total_n
-  total_column <- rowSums(x[, -1], na.rm = TRUE)
-  total_row <- c(colSums(x[, -1], na.rm = TRUE), total_n)
+  total_column <- rowSums(x[numeric_columns], na.rm = TRUE)
+  total_row <- c(colSums(x[numeric_columns], na.rm = TRUE), total_n)
 
   # proportions?
   props <- attributes(x)$proportions
@@ -113,16 +119,16 @@ format.datawizard_crosstab <- function(x, format = "text", digits = 1, big_mark 
     tmp <- x
     if (identical(props, "row")) {
       for (i in seq_len(nrow(x))) {
-        row_sum <- sum(x[i, -1], na.rm = TRUE)
+        row_sum <- sum(x[i, numeric_columns], na.rm = TRUE)
         if (row_sum == 0) {
           row_sum_string <- "(0%)"
         } else {
-          row_sum_string <- sprintf("(%.*f%%)", digits, 100 * x[i, -1] / row_sum)
+          row_sum_string <- sprintf("(%.*f%%)", digits, 100 * x[i, numeric_columns] / row_sum)
         }
-        tmp[i, -1] <- paste(format(x[i, -1]), format(row_sum_string, justify = "right"))
+        tmp[i, numeric_columns] <- paste(format(x[i, numeric_columns]), format(row_sum_string, justify = "right"))
       }
     } else if (identical(props, "column")) {
-      for (i in seq_len(ncol(x))[-1]) {
+      for (i in seq_len(ncol(x))[numeric_columns]) {
         col_sum <- sum(x[, i], na.rm = TRUE)
         if (col_sum == 0) {
           col_sum_string <- "(0%)"
@@ -132,7 +138,7 @@ format.datawizard_crosstab <- function(x, format = "text", digits = 1, big_mark 
         tmp[, i] <- paste(format(x[, i]), format(col_sum_string, justify = "right"))
       }
     } else if (identical(props, "full")) {
-      for (i in seq_len(ncol(x))[-1]) {
+      for (i in seq_len(ncol(x))[numeric_columns]) {
         tmp[, i] <- paste(
           format(x[, i]),
           format(sprintf("(%.*f%%)", digits, 100 * x[, i] / total_n), justify = "right")
@@ -154,22 +160,29 @@ format.datawizard_crosstab <- function(x, format = "text", digits = 1, big_mark 
   })
   # Remove ".00" from numbers
   ftab$Total <- gsub("\\.00$", "", as.character(total_column))
-  # for text format, insert "empty row" before last total row
-  if (identical(format, "text") || identical(format, "markdown")) {
-    empty_row <- as.data.frame(t(data.frame(
-      rep("", ncol(ftab)),
-      c("Total", as.character(total_row)),
-      stringsAsFactors = FALSE
-    )))
-  } else {
-    empty_row <- as.data.frame(t(data.frame(
-      c("Total", as.character(total_row)),
-      stringsAsFactors = FALSE
-    )))
+
+  # add final total row to each sub-table. For multiple, collapsed table
+  # (i.e. when length of `by` > 1), we don't want multiple total rows in the
+  # table, so we would set include_total_row = FALSE for objects of class
+  # `datawizard_crosstabs` (note plural s!)
+  if (include_total_row) {
+    # for text format, insert "empty row" before last total row
+    if (identical(format, "text") || identical(format, "markdown")) {
+      empty_row <- as.data.frame(t(data.frame(
+        rep("", ncol(ftab)),
+        c("Total", as.character(total_row)),
+        stringsAsFactors = FALSE
+      )))
+    } else {
+      empty_row <- as.data.frame(t(data.frame(
+        c("Total", as.character(total_row)),
+        stringsAsFactors = FALSE
+      )))
+    }
+    colnames(empty_row) <- colnames(ftab)
+    ftab <- rbind(ftab, empty_row)
+    ftab[nrow(ftab), ] <- gsub("\\.00$", "", ftab[nrow(ftab), ])
   }
-  colnames(empty_row) <- colnames(ftab)
-  ftab <- rbind(ftab, empty_row)
-  ftab[nrow(ftab), ] <- gsub("\\.00$", "", ftab[nrow(ftab), ])
 
   # insert big marks?
   ftab$Total <- .add_commas_in_numbers(ftab$Total, big_mark)
@@ -266,9 +279,21 @@ print_html.datawizard_crosstabs <- function(x, big_mark = NULL, ...) {
         i$groups <- paste0("Grouped by ", i[["Group"]][1])
         i$Group <- NULL
       }
-      format(i, format = "html", big_mark = big_mark, ...)
+      # first variable differs for each data frame, so we harmonize it here
+      if (is.null(i$groups)) {
+        grp_variable <- "groups"
+      } else {
+        grp_variable <- "Variable"
+      }
+      i[[grp_variable]] <- colnames(i)[1]
+      colnames(i)[1] <- "Value"
+      # move column to first position
+      i <- data_relocate(i, select = grp_variable, before = 1)
+      # format data frame
+      format(i, format = "html", big_mark = big_mark, include_total_row = FALSE, ...)
     })
 
+    # now reorder and bind
     out <- do.call(rbind, x)
 
     # print table
