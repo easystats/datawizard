267fda9857dc730f6762317ed166201da08c3baa
Daniel <mail@danielluedecke.de>
2025-04-01 19:05:03 +0200

    add test

--- tests/testthat/test-data_tabulate.R
+++ tests/testthat/test-data_tabulate.R
@@ -370,6 +370,16 @@ test_that("data_tabulate, cross tables, errors weights", {
   expect_error(data_tabulate(efc$c172code, weights = efc$wweight), regex = "not found")
 })
 
+test_that("data_tabulate, cross tables, modify structure", {
+  data(efc, package = "datawizard")
+  x <- data_group(x, c("c172code", "e16sex"))
+  out <- data_tabulate(x, "c172code")
+  out[] <- lapply(out, data_select, exclude = c("Variable", "Raw %", "Cumulative %"))
+  junk <- capture.output(print_md(out))
+  expect_false(grepl("Variable", junk[1], fixed = TRUE))
+  expect_false(grepl("Raw %", junk[1], fixed = TRUE))
+})
+
 
 # markdown -------------------------
 
