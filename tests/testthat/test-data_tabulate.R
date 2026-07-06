1a7ee3728d8ed81ebb34f5a6d2a257732804fa04
Elin Waring <elin.waring@gmail.com>
2026-07-06 00:47:59 -0400

    Add test and update snapshots.

--- tests/testthat/test-data_tabulate.R
+++ tests/testthat/test-data_tabulate.R
@@ -1031,6 +1031,10 @@ test_that("data_tabulate, table methods", {
   x <- data_tabulate(mtcars$cyl, mtcars$gear)
   expect_type(as.table(x), "list")
   expect_s3_class(as.table(x, simplify = TRUE), "table")
+  expect_identical(
+    names(dimnames(as.table(x)[[1]])),
+    c("mtcars$cyl", "mtcars$gear")
+  )
   expect_snapshot(as.table(x))
   expect_snapshot(as.table(x, simplify = TRUE))
 
